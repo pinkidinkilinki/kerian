5b0f6128d870e58cc35e9db8f8f3ef10510c2f6e
Kristiyan Kovachev <krkfris@gmail.com>
2026-04-23 13:38:35 +0300

    Remove duplicate cake entries from catalogue

--- public/catalogue_daily.js
+++ public/catalogue_daily.js
@@ -5,11 +5,7 @@ const rawData = [
 { id: "M003", price1: 4 ,price2: 2, title_sizes: "Торта Гараш - 16", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/3_pbsix9.webp", category: "daily" },
 { id: "M004", price1: 5.2 ,price2: 2.6, title_sizes: "Празнична с макарони - 25 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/1_ms8put.webp", category: "daily" },
 { id: "M005", price1: 5.2 ,price2: 2.6, title_sizes: "Цифра с коли - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921423/2_lrcp8b.webp", category: "daily" },
-{ id: "M0011", price1: 4.5 ,price2: 2.3, title_sizes: "Пари - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251018_141850679_wxtpej.webp", category: "daily" },
-{ id: "M0012", price1: 4 ,price2: 2, title_sizes: "Снимка Роналдо - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251227_123412796_o67zn8.webp", category: "daily" },
-{ id: "M0013", price1: 4 ,price2: 2, title_sizes: "Торта Гараш - 16", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/3_pbsix9.webp", category: "daily" },
-{ id: "M0014", price1: 5.2 ,price2: 2.6, title_sizes: "Празнична с макарони - 25 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/1_ms8put.webp", category: "daily" },
-{ id: "M0015", price1: 5.2 ,price2: 2.6, title_sizes: "Цифра с коли - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921423/2_lrcp8b.webp", category: "daily" },
+
 
 
 
