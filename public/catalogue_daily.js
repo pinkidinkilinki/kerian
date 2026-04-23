45d5c49e1389b02fe780c0ee8e01c37ed19f3a47
Kristiyan Kovachev <krkfris@gmail.com>
2026-04-23 17:51:17 +0300

    Update catalogue_daily.js

--- public/catalogue_daily.js
+++ public/catalogue_daily.js
@@ -1,10 +1,21 @@
 // catalogue_daily.js — Standalone data for today's cakes
 const rawData = [
-  { id: "M001", price1: 4.5 ,price2: 2.3, title_sizes: "Пари - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251018_141850679_wxtpej.webp", category: "daily" },
-{ id: "M002", price1: 4 ,price2: 2, title_sizes: "Снимка Роналдо - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251227_123412796_o67zn8.webp", category: "daily" },
-{ id: "M003", price1: 4 ,price2: 2, title_sizes: "Торта Гараш - 16", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/3_pbsix9.webp", category: "daily" },
-{ id: "M004", price1: 5.2 ,price2: 2.6, title_sizes: "Празнична с макарони - 25 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/1_ms8put.webp", category: "daily" },
-{ id: "M005", price1: 5.2 ,price2: 2.6, title_sizes: "Цифра с коли - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921423/2_lrcp8b.webp", category: "daily" },
+{ id: "M001", price1: 4.6 ,price2: 2.3, title_sizes: "Торта с пари декорация - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251018_141850679_wxtpej.webp", category: "daily" },
+{ id: "M002", price1: 4 ,price2: 2, title_sizes: "Фотодекорация Кристиано Роналдо - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1767533260/IMG_20251227_123412796_o67zn8.webp", category: "daily" },
+{ id: "M003", price1: 4 ,price2: 2, title_sizes: "Торта Гараш класическа - 16", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/3_pbsix9.webp", category: "daily" },
+{ id: "M004", price1: 5.2 ,price2: 2.6, title_sizes: "Празнична торта с макарони - 25 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921420/1_ms8put.webp", category: "daily" },
+{ id: "M005", price1: 5.2 ,price2: 2.6, title_sizes: "Торта цифра с коли детска - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776921423/2_lrcp8b.webp", category: "daily" },
+{ id: "M006", price1: 4.6 ,price2: 2.3, title_sizes: "Торта Дантела с рози - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952818/4_copy_wsbpvv.webp", category: "daily" },
+{ id: "M007", price1: 4.2 ,price2: 2.1, title_sizes: "Брауни кекс шоколадов - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952817/30_copy_bodkg4.webp", category: "daily" },
+{ id: "M008", price1: 4 ,price2: 2, title_sizes: "Фотодекорация рок група по снимка - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952810/19_copy_arx1l0.webp", category: "daily" },
+{ id: "M009", price1: 5.2 ,price2: 2.6, title_sizes: "Торта Подарък - 30 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952805/6_copy_k1dhbl.webp", category: "daily" },
+{ id: "M010", price1: 5.2 ,price2: 2.6, title_sizes: "Сандъче с пари и бял шоколад - 20 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952807/10_copy_ygpcx2.webp", category: "daily" },
+{ id: "M011", price1: 4 ,price2: 2, title_sizes: "Фотодекорация Лило и Стич детска - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952805/5_copy_w4gscs.webp", category: "daily" },
+{ id: "M012", price1: 5.2 ,price2: 2.6, title_sizes: "Торта розови феи с фигурки и макарони - 25 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952814/24_copy_wsqwmw.webp", category: "daily" },
+{ id: "M013", price1: 4 ,price2: 2, title_sizes: "Фотодекорация Адвенчър Тайм с плодове - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952808/12_copy_emzzqj.webp", category: "daily" },
+{ id: "M014", price1: 4 ,price2: 2, title_sizes: "Фотодекорация Пес Патрул детска - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952806/8_copy_zdakfr.webp", category: "daily" },
+{ id: "M015", price1: 4 ,price2: 2, title_sizes: "Фотодекорация Тик Ток - 16 +", img: "https://res.cloudinary.com/dsn8jkh0o/image/upload/v1776952809/15_copy_hn1znh.webp", category: "daily" },
+
 
 
 
